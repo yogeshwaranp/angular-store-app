a2be6174b815ecc3ce1b232fba7d29c1bc8fe9c8
yogeshwaranp <yogeshwaran0609@gmail.com>
2021-11-16 20:07:49 +0530

    2 files modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { ProductListComponent } from './product-list/product-list.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
 import {CartService} from './cart.service';
+import { CartComponent } from './cart/cart.component';
 
 @NgModule({
   imports: [
@@ -17,6 +18,7 @@ import {CartService} from './cart.service';
     RouterModule.forRoot([
       { path: '', component: ProductListComponent },
       { path: 'products/:productId', component: ProductDetailsComponent },
+      {path:'cart',component:CartComponent},
     ]),
   ],
   declarations: [
@@ -25,6 +27,7 @@ import {CartService} from './cart.service';
     ProductListComponent,
     ProductAlertsComponent,
     ProductDetailsComponent,
+    CartComponent
   ],
 
   providers: [CartService], 
---
+++ src/app/cart/cart.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+import { CartService } from '../cart.service';
+
+@Component({
+  selector: 'app-cart',
+  templateUrl: './cart.component.html',
+  styleUrls: ['./cart.component.css'],
+})
+export class CartComponent implements OnInit {
+  constructor(private cartservice: CartService) {}
+
+  ngOnInit() {}
+}
